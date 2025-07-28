f88cb7af61d4962c50199a06a0c09345e5259110
Alfredo Rios <riosalf15@gmail.com>
2025-07-28 13:56:13 -0300

    Template list sin rutas

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from "./shared/header/header";
+import { Footer } from "./shared/footer/footer";
+import { Portfolio } from "./pages/portfolio/portfolio";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header, Footer, Portfolio],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
